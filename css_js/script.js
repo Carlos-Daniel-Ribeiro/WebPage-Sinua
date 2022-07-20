d46d164710f3a8c3a4a7c7784cf4c482d40615ae
Carlos Daniel <carlositu33@gmail.com>
2022-07-20 18:26:23 -0300

    klkl

--- css_js/script.js
+++ css_js/script.js
@@ -9,3 +9,27 @@ function decidir_scroll() {
     }
 }
 window.addEventListener('scroll', decidir_scroll);
+
+function menuTouch() {
+    let menuBar = document.getElementById("menu-bar");
+    menuBar.classList.remove("menu-bar")
+    menuBar.classList.add("menu-bar2")
+    let navRes = document.getElementById("nav_res");
+    navRes.style.width = "200px"
+    let menuBar2 = document.getElementById("menu-bar2");
+    menuBar2.style.display = "block"
+    let menuContent = document.getElementById("menu_content");
+    menuContent.style.display = "block"
+}
+
+function menuTouch2() {
+    let menuBar = document.getElementById("menu-bar");
+    menuBar.classList.remove("menu-bar2")
+    menuBar.classList.add("menu-bar")
+    let navRes = document.getElementById("nav_res");
+    navRes.style.width = "0px"
+    let menuBar2 = document.getElementById("menu-bar2");
+    menuBar2.style.display = "none"
+    let menuContent = document.getElementById("menu_content");
+    menuContent.style.display = "none"
+}
