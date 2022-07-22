6eee938029f3dc05d3550bed324515d31d9761be
Carlos Daniel <carlositu33@gmail.com>
2022-07-22 20:33:04 -0300

    klkl

--- css_js/script.js
+++ css_js/script.js
@@ -9,7 +9,6 @@ function decidir_scroll() {
     }
 }
 window.addEventListener('scroll', decidir_scroll);
-
 function menuTouch() {
     let menuBar = document.getElementById("menu-bar");
     menuBar.classList.remove("menu-bar")
@@ -21,7 +20,6 @@ function menuTouch() {
     let menuContent = document.getElementById("menu_content");
     menuContent.style.display = "block"
 }
-
 function menuTouch2() {
     let menuBar = document.getElementById("menu-bar");
     menuBar.classList.remove("menu-bar2")
@@ -33,7 +31,6 @@ function menuTouch2() {
     let menuContent = document.getElementById("menu_content");
     menuContent.style.display = "none"
 }
-
 function trocarImg() {
     let picture = document.getElementById("picture");
     if ( picture.classList.contains("img1")) {
@@ -48,3 +45,115 @@ function trocarImg() {
     }
 }
 setInterval(trocarImg,7000);
+function botao1() {
+    let met1 = document.querySelector("#d1");
+    met1.style.display = "block";
+    let met2 = document.querySelector("#d2");
+    met2.style.display = "none";
+    let met3 = document.querySelector("#d3");
+    met3.style.display = "none";
+    let bot1 = document.querySelector("#bb1");
+    bot1.classList.add("active")
+    let bot2= document.querySelector("#bb2");
+    bot2.classList.remove("active")
+    let bot3 = document.querySelector("#bb3");
+    bot3.classList.remove("active")
+}
+function botao2() {
+    let met1 = document.querySelector("#d1");
+    met1.style.display = "none";
+    let met2 = document.querySelector("#d2");
+    met2.style.display = "block";
+    let met3 = document.querySelector("#d3");
+    met3.style.display = "none";
+    let bot1 = document.querySelector("#bb1");
+    bot1.classList.remove("active")
+    let bot2= document.querySelector("#bb2");
+    bot2.classList.add("active")
+    let bot3 = document.querySelector("#bb3");
+    bot3.classList.remove("active")
+}
+function botao3() {
+    let met1 = document.querySelector("#d1");
+    met1.style.display = "none";
+    let met2 = document.querySelector("#d2");
+    met2.style.display = "none";
+    let met3 = document.querySelector("#d3");
+    met3.style.display = "block";
+    let bot1 = document.querySelector("#bb1");
+    bot1.classList.remove("active")
+    let bot2= document.querySelector("#bb2");
+    bot2.classList.remove("active")
+    let bot3 = document.querySelector("#bb3");
+    bot3.classList.add("active")
+}
+function botao2_1() {
+    let met1 = document.querySelector("#t1");
+    met1.style.display = "flex";
+    let met2 = document.querySelector("#t2");
+    met2.style.display = "none";
+    let met3 = document.querySelector("#t3");
+    met3.style.display = "flex";
+    let met4 = document.querySelector("#t4");
+    met4.style.display = "none";
+    let bot1 = document.querySelector("#b11");
+    bot1.classList.add("active2")
+    let bot2= document.querySelector("#b22");
+    bot2.classList.remove("active2")
+}
+function botao2_2() {
+    let met1 = document.querySelector("#t1");
+    met1.style.display = "none";
+    let met2 = document.querySelector("#t2");
+    met2.style.display = "flex";
+    let met3 = document.querySelector("#t3");
+    met3.style.display = "none";
+    let met4 = document.querySelector("#t4");
+    met4.style.display = "flex";
+    let bot1 = document.querySelector("#b11");
+    bot1.classList.remove("active2")
+    let bot2= document.querySelector("#b22");
+    bot2.classList.add("active2")
+}
+function botao3_1() {
+    let met1 = document.querySelector("#p1");
+    met1.style.display = "flex";
+    let met2 = document.querySelector("#p2");
+    met2.style.display = "none";
+    let met3 = document.querySelector("#p3");
+    met3.style.display = "none";
+    let bot1 = document.querySelector("#b111");
+    bot1.classList.add("active3")
+    let bot2= document.querySelector("#b222");
+    bot2.classList.remove("active3")
+    let bot3= document.querySelector("#b333");
+    bot3.classList.remove("active3")
+}
+function botao3_2() {
+    let met1 = document.querySelector("#p1");
+    met1.style.display = "none";
+    let met2 = document.querySelector("#p2");
+    met2.style.display = "flex";
+    let met3 = document.querySelector("#p3");
+    met3.style.display = "none";
+    let bot1 = document.querySelector("#b111");
+    bot1.classList.remove("active3")
+    let bot2= document.querySelector("#b222");
+    bot2.classList.add("active3")
+    let bot3= document.querySelector("#b333");
+    bot3.classList.remove("active3")
+}
+function botao3_3() {
+    let met1 = document.querySelector("#p1");
+    met1.style.display = "none";
+    let met2 = document.querySelector("#p2");
+    met2.style.display = "none";
+    let met3 = document.querySelector("#p3");
+    met3.style.display = "flex";
+    let bot1 = document.querySelector("#b111");
+    bot1.classList.remove("active3")
+    let bot2= document.querySelector("#b222");
+    bot2.classList.remove("active3")
+    let bot3= document.querySelector("#b333");
+    bot3.classList.add("active3")
+}
